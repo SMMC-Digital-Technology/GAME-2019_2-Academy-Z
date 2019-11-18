a215a7b284c9199c7d151ad12542fb2dec43b70d
NickDowling57 <dowlinne@mackillop.act.edu.au>
2019-11-19 10:10:46 +1100

    Sprite Sheet and Button Code
    Applying code to the button to change the background screen

--- game.js
+++ game.js
@@ -1,38 +1,45 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Acadamey Z', { preload: preload, create: create, update: update });
-var ground
+var game = new Phaser.Game(800,600, Phaser.CANVAS, 'theGame', { preload: preload, create: create, update: update });
+var button;
+var backgrounds;
 
 function preload() {
-  game.load.image('BG', 'assets/Background.png');
+  game.load.spritesheet('BG', 'assets/Backgrounds.png');
   game.load.image('Z1', 'assets/Zombie.png');
   game.load.image('Z2', 'assets/Zombie 2.png');
   game.load.image('Z3', 'assets/Zombie 3.png');
   game.load.image('Z4', 'assets/Zombie 4.png');
   game.load.image('Z5', 'assets/Zombie 5.png');
   game.load.image('STEEL', 'assets/STEEL.png');
-  game.load.image('BBT STEEL', 'assets/STEEL BIG.png');
-  game.load.image('ground', 'assets/platforms.png')
+  game.load.image('Button', 'assets/Button.png');
 
 }
 
 function create() {
  game.physics.startSystem(Phaser.Physics.ARCADE);
-  platforms = game.add.group();
-  platforms.enableBody = true;
-  game.add.sprite(0,0 , 'BG');
 
-  button = game.add.button(game.world.centerX, game.world.centerY, 'button');
-  button.onInputUp.add(myFunction);
 
-  button = game.add.button(game.world.centerX, game.world.centerY, 'button');
+ backgrounds = game.add.tileSprite(0, 0, 800, 600, 'BG');
+
+
+  button = game.add.button(game.world.centerX, game.world.centerY, 'Button', actionOnClick, this, 2, 1, 0);
 button.anchor.setTo(0.5,0.5);
-button.onInputUp.add(myFunction);
 var text = game.add.text(button.x,button.y,'Start');
 text.anchor.setTo(0.5,0.5);
 
+    /*button.onInputOver.add(over, this);
+    button.onInputOut.add(out, this);
+    button.onInputUp.add(up, this);*/
+}
 
 
+
+
+function update() {
 
 }
 
-function update() {
+function actionOnClick () {
+
+    backgrounds.visible =! backgrounds.visible;
+
 }
